d401dcb22ed77657aa9f43c405dccdac5923694d
Salmi Konsta S <kxsalmi@cubbli20-gold.local>
2022-06-28 17:06:58 +0300

    tehtävä 1.1

--- osa1/t_app/src/App.js
+++ osa1/t_app/src/App.js
@@ -9,17 +9,39 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1}/>
+      <Content part={part2} exercises={exercises2}/>
+      <Content part={part3} exercises={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>
+        {props.course}
+      </h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
       <p>
-        {part1} {exercises1}
+        {props.part} {props.exercises}
       </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>  
     </div>
   )
 }
