5783b3d292899f1b52e8656415c36051547aab04
Salmi Konsta S <kxsalmi@cubbli20-gold.local>
2022-07-01 18:31:35 +0300

    tehtävä 1.4 korjaus

--- osa1/t_app/src/App.js
+++ osa1/t_app/src/App.js
@@ -20,9 +20,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={parts[0]['name']} part2={parts[1]['name']} part3={parts[2]['name']} 
-      exercises1={parts[0]['exercises']} exercises2={parts[1]['exercises']} exercises3={parts[2]['exercises']}/>
-      <Total exercises1={parts[0]['exercises']} exercises2={parts[1]['exercises']} exercises3={parts[2]['exercises']}/>
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
@@ -50,9 +49,9 @@ const Content = (props) => {
   return (
     <div>
       <p>
-        <Part part={props.part1} exercises={props.exercises1}/>
-        <Part part={props.part2} exercises={props.exercises2}/>
-        <Part part={props.part3} exercises={props.exercises3}/>
+        <Part part={props.parts[0]['name']} exercises={props.parts[0]['exercises']}/>
+        <Part part={props.parts[1]['name']} exercises={props.parts[1]['exercises']}/>
+        <Part part={props.parts[2]['name']} exercises={props.parts[2]['exercises']}/>
       </p>
     </div>
   )
@@ -61,7 +60,7 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>  
+      <p>Number of exercises {props.parts[0]['exercises'] + props.parts[1]['exercises'] + props.parts[2]['exercises']}</p>  
     </div>
   )
 }
